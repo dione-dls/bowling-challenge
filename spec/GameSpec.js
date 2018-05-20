bbd763b8cfacb64a5282c93e6efa1d62602ac999
Dione Estabillo <dione.estabillo@gmail.com>
2018-05-20 15:31:19 +0100

    Added roll and score functions

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -1,5 +1,13 @@
 describe('Game', function() {
-  it('can create a game', function () {
+  it('can create a game', function() {
     var game = new Game();
+  });
+
+  it('can roll gutter game', function() {
+    var game = new Game();
+    for (var i = 0; i < 20; i++) {
+      game.roll(0);
+    }
+    expect(game.score()).toEqual(0);
   });
 });
