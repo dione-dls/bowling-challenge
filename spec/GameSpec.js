37e2e88c743c02c4353065f7ce740d24349b9bcd
Dione Estabillo <dione.estabillo@gmail.com>
2018-05-20 15:54:33 +0100

    Refactored test

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -1,13 +1,22 @@
 describe('Game', function() {
-  it('can create a game', function() {
-    var game = new Game();
+
+  var game;
+
+  beforeEach(function() {
+    game = new Game();
   });
 
   it('can roll gutter game', function() {
-    var game = new Game();
     for (var i = 0; i < 20; i++) {
       game.roll(0);
     }
     expect(game.score()).toEqual(0);
+  });
+
+  it('can roll all ones', function() {
+    for(var i = 0; i < 20; i++) {
+      game.roll(1);
+    }
+    expect(game.score()).toEqual(20);
   });
 });
